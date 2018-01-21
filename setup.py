40c01ad66f58791d571d66f1b5cfbdb21e29a9db
kalwe-caramalac <kalwe123@hotmail.com>
2018-01-21 16:47:30 -0300

    add py configs and requirements files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+dependencies = [
+    'requests'
+]
+
+version = '0.0.1'
+
+setup(name='Jezebel',
+    version=version,
+    description='Jezebel is working to you get rich, bitch!',
+    author='Kalwe Caramalac',
+    email='kalwe.dev@gmail.com',
+    url='',
+    license='',
+    install_requires=dependencies,
+    keywords=['bitcoin','rich','jezebel']
+)

14c1d8cc78d231b5fdedbde774315ac597bd5d34
kalwe-caramalac <kalwe123@hotmail.com>
2018-01-28 02:30:44 -0300

    add changes in dunder init tests

--- jezebel/test/riskmanager/__init__.py
+++
@@ -1,6 +0,0 @@
-# maybe this code needed to be change to one directory above
-# import os, sys
-
-# testdir = os.path.dirname(__file__)
-# srcdir = '../../'
-# sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
